9d5fb5be32d820df5ae7a4b22310d8c7a957559e
joviedog <55594131+joviedog@users.noreply.github.com>
2021-04-08 19:12:46 -0500

    Delete CreateData.py

--- CreateData.py
+++
@@ -1,8 +0,0 @@
-from dataScrambler import DataCreator
-from time import perf_counter
-
-init_time = perf_counter()
-creator = DataCreator()
-creator.createData(1000, 50)
-end_time = perf_counter()
-print("1000 preguntas cada una con 50 comentarios en {} segundos".format(end_time-init_time))
