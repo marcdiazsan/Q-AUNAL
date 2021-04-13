f087c32900acee0b6c54d29cf8b025cd2afa8f2f
Julian Sanchez <juliandavidsanchezsanabria@gmail.com>
2021-04-13 18:02:27 -0500

    Modificacion en la generacion de datos, se añade numero de likes a pregunta, se genera una lista de temas y se modifican las fechas

--- CreateData.py
+++ CreateData.py
@@ -3,6 +3,6 @@ from time import perf_counter
 
 init_time = perf_counter()
 creator = DataCreator()
-creator.createData(1000, 50)
+creator.createData(1000, 10)
 end_time = perf_counter()
 print("1000 preguntas cada una con 50 comentarios en {} segundos".format(end_time-init_time))
--- dataScrambler.py
+++ dataScrambler.py
@@ -2,22 +2,29 @@ from faker import Faker
 import json
 import random
 from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class DataCreator():
     def __init__(self):
         pass
+
     def createData(self, preguntas, comentarios):
         # Write to file
         fake = Faker("en_US")
+        temas = ["Ingenieria", "Artes", "Ciencias", "Ciencias Agrarias", "Ciencias Economicas", "Ciencias Humanas", "Derecho, Ciencias Politicas y Sociales", "Enfermeria", "Medicina", "Medicina Veterinaria y de Zootecnia", "Odontologia"]
+        estatus = ["Activo", "Cerrado"]
+        fecha = datetime.now() - timedelta(468)
+        duracion = preguntas//468
         data = {}
         for i in range(1, preguntas):
-            pregunta = {"preg_id": i, "titulo": "Titulo {}".format(i), "texto":fake.text(), "date":str(datetime.now()), "tema":"Ingenieria", "userid": randint(1,200), "estatus": "Success", "comments":[]}
-            for j in range(1,randint(1,comentarios)):
-                comentario = {"com_id": str(i) + str(j), "texto":fake.text(), "date":str(datetime.now()), "userid": randint(1,200), "likes": randint(0, 100), "util": "True", "preg_id":i}
+            if (i % 468) == 0 and i >= 0:
+                fecha += timedelta(1)
+            pregunta = {"preg_id": i, "titulo": "Titulo {}".format(i), "texto": fake.text(), "date": str(fecha), "tema": random.choice(temas), "userid": randint(1, 200), "likes": randint(0, 100), "estatus": random.choice(estatus), "comments": []}
+            for j in range(1, randint(1, comentarios)):
+                comentario = {"com_id": str(i) + str(j), "texto":fake.text(), "date": str(fecha), "userid": randint(1, 200), "likes": randint(0, 100), "util": "True", "preg_id": i}
                 pregunta["comments"].append(comentario)
-            data[i]= pregunta
+            data[i] = pregunta
         JSONData = open('JSONData.json', 'w')
         JSONData.write(json.dumps(data))
         JSONData.close()
