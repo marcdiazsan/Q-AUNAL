7faa2beb6d1482c92d589a40dc253abe66d3ab1a
Juan Esteban Oviedo <joviedog@unal.edu.co>
2021-05-20 18:15:52 -0500

    Webapp init commit - Awaiting FrontEnd

---
+++ webapp/qaunal/forms.py
@@ -0,0 +1,62 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from qaunal.models import User
+
+
+class RegistrationForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired(message='Este campo es obligatorio'), Length(
+        min=10, message="El nombre es demasiado corto")])
+    email = StringField('Email', validators=[DataRequired(
+        message='Este campo es obligatorio'), Email(message='Dirección de correo no válida')])
+    password = PasswordField('Contraseña', validators=[
+                             DataRequired(message='Este campo es obligatorio')])
+    password_confirmation = PasswordField(
+        'Confirmar Contraseña', validators=[DataRequired(message='Este campo es obligatorio'), EqualTo('password', message='Las contraseñas no coinciden')])
+    submit = SubmitField('Registrarse')
+
+    # Validar que el correo no exista ya en la base de usuarios
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                "El correo ya está registrado. Intente iniciando sesión")
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(
+        message='Este campo es obligatorio'), Email(message='Dirección de correo invalida')])
+    password = PasswordField('Contraseña', validators=[
+                             DataRequired(message='Este campo es obligatorio')])
+    remember = BooleanField('Recuerdame')
+    submit = SubmitField('Iniciar Sesion')
+
+
+class CreateQuestionForm(FlaskForm):
+    opcionesEstatus = [('Resuelta', 'Resuelta'),
+                       ('No Resuelta', 'No Resuelta')]
+    etiquetasTema = [('Artes', 'Artes'), 
+                     ('Ingenieria', 'Ingenieria'), 
+                     ('Ciencias', 'Ciencias'),
+                     ('Ciencias Economicas', 'Ciencias Economicas'),
+                     ('Medicina', 'Medicina'),
+                     ('Medicina Veterinaria y de Zootecnia',
+                      'Medicina Veterinaria y de Zootecnia'),
+                     ('Ciencias Agrarias', 'Ciencias Agrarias'),
+                     ('Ciencias Humanas', 'Ciencias Humanas'), 
+                     ('Derecho, Ciencias Políticas y Sociales', 'Derecho, Ciencias Políticas y Sociales'), 
+                     ('Enfermeria', 'Enfermeria'),
+                     ('Odontologia', 'Odontologia')]
+    titulo = StringField('Titulo', validators=[
+                         DataRequired()])
+    contenido = TextAreaField('Contenido', validators=[
+        DataRequired(message='Este campo es obligatorio')])
+    tema = SelectField('Tema', choices=etiquetasTema)
+    estatus = SelectField('Estatus', choices=opcionesEstatus)
+    submit = SubmitField('Crear Pregunta')
+
+
+class CreateCommentForm(FlaskForm):
+    contenido = TextAreaField('Contenido', validators=[DataRequired(
+        message='Este campo es obligatorio'), Length(min=2)])
+    submit = SubmitField('Añadir Comentario')
---
+++ webapp/qaunal/routes.py
@@ -0,0 +1,218 @@
+from qaunal.estructuras import SLL_Stack
+from flask import jsonify, request, render_template, url_for, flash, redirect, abort
+from qaunal import app, db, bcrypt
+from qaunal.models import User, Pregunta, Comentario
+from qaunal.forms import RegistrationForm, LoginForm, CreateQuestionForm, CreateCommentForm
+from flask_login import login_user, current_user, logout_user, login_required
+from qaunal.estructuras import SLL_Stack
+
+@app.route('/')
+def dashboardPrincipal():
+    page =  request.args.get('page', default=1, type=int)
+    preguntas = Pregunta.query.all()
+    # Uso E.D
+    stackPreguntas = SLL_Stack()
+    for preg in preguntas:
+        stackPreguntas.push(preg)
+    # Tamaño del stack de preguntas
+    current = stackPreguntas.getHead()
+    size = 0
+    while current:
+        size += 1
+        current = current.getNext()
+    return render_template('home.html', titulo='Inicio', preguntas=stackPreguntas, size=size)
+
+
+@app.route('/registrarse', methods=['GET', 'POST'])
+def registroUsuario():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboardPrincipal'))
+
+    forma = RegistrationForm()
+    if forma.validate_on_submit():
+        # Registro de un usuario nuevo
+        hashed_password = bcrypt.generate_password_hash(
+            forma.password.data).decode('utf-8')
+        user = User(nombre=forma.nombre.data,
+                    email=forma.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        # Dar un mensaje al usuario
+        flash('Bienvenido a QA UNAL, {}!. Inicia sesión!'.format(
+            forma.nombre.data), 'success')
+        return redirect(url_for('iniciarSesion'))
+    return render_template('registroUsuario.html', forma=forma, titulo='Registro')
+
+
+@app.route('/iniciar_sesion', methods=['GET', 'POST'])
+def iniciarSesion():
+    forma = LoginForm()
+    if forma.validate_on_submit():
+        user = User.query.filter_by(email=forma.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, forma.password.data):
+            login_user(user, remember=forma.remember.data)
+            next_page = request.args.get('next')
+            flash('Bienvenido de vuelta!', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('dashboardPrincipal'))
+        else:
+            flash('Correo o contraseña errados. Revise los datos', 'danger')
+    return render_template('iniciarSesion.html', forma=forma, titulo='Inicio Sesión')
+
+
+@app.route('/preguntas/nueva', methods=['GET', 'POST'])
+@login_required
+def nuevaPregunta():
+    forma = CreateQuestionForm()
+    if forma.validate_on_submit():
+        status = True if forma.estatus.data == 'Resuelta' else False
+        pregunta = Pregunta(titulo=forma.titulo.data, contenido=forma.contenido.data, etiquetas=forma.tema.data, resuelta=status, likes=0, user_id=current_user.id)
+        db.session.add(pregunta)
+        db.session.commit()
+        flash('Tu pregunta se ha creado!', 'success')
+        return redirect(url_for('dashboardPrincipal'))
+    return render_template('nuevaPregunta.html', forma=forma, titulo='Nueva Pregunta', legenda='¿Qué duda tienes?')
+
+
+@app.route('/preguntas/<int:preg_id>', methods=['GET','POST'])
+@login_required
+def pregunta(preg_id):
+    pregunta = Pregunta.query.get_or_404(preg_id)
+    # Implementación E:D
+    comentariosPregunta = SLL_Stack()
+    for comentario in pregunta.comentarios:
+        comentariosPregunta.push(comentario)
+
+    # Tamaño del arreglo de comentarios
+    size = 0
+    current = comentariosPregunta.getHead()
+    while current:
+        current = current.getNext()
+        size += 1
+    return render_template('pregunta.html', titulo=Pregunta.titulo, pregunta=pregunta, comentarios=comentariosPregunta, size=size)
+
+
+@app.route('/preguntas/<int:preg_id>/actualizar', methods=['GET', 'POST'])
+@login_required
+def actualizarPregunta(preg_id):
+    pregunta = Pregunta.query.get_or_404(preg_id)
+    if pregunta.autor != current_user:
+        abort(403)
+    forma = CreateQuestionForm()
+    if forma.validate_on_submit():
+        pregunta.titulo = forma.titulo.data
+        pregunta.contenido = forma.contenido.data
+        pregunta.etiquetas = forma.tema.data
+        pregunta.resuelta = True if forma.estatus.data == 'Resuelta' else False
+        db.session.commit()
+        flash('Actualizamos tu pregunta', 'success')
+        return redirect(url_for('pregunta', preg_id=pregunta.id))
+    elif request.method == 'GET':
+    # Llenar la forma con los datos actuales
+        forma.titulo.data = pregunta.titulo
+        forma.contenido.data =pregunta.contenido
+        forma.tema.data = pregunta.etiquetas
+        forma.estatus.data = pregunta.resuelta
+        return render_template('nuevaPregunta.html', forma=forma, titulo='Acualizar Pregunta',
+        legenda='Cambia tu pregunta')
+
+
+@app.route('/preguntas/<int:preg_id>/eliminar', methods=['POST'])
+@login_required
+def eliminarPregunta(preg_id):
+    pregunta = Pregunta.query.get_or_404(preg_id)
+    if pregunta.autor != current_user:
+        abort(403)
+    db.session.delete(pregunta)
+    db.session.commit()
+    flash('Hemos eliminado tu pregunta!', 'success')
+    return redirect(url_for('dashboardPrincipal'))
+
+
+@ app.route('/like/pregunta/<int:preg_id>', methods=['GET', 'POST'])
+def likePregunta(preg_id):
+    pregunta = Pregunta.query.filter_by(id=preg_id).first()
+    pregunta.likes = pregunta.likes + 1
+    db.session.commit()
+    return redirect(url_for('pregunta', preg_id=pregunta.id))
+
+@app.route('/comentarios/<int:com_id>', methods=['GET', 'POST'])
+@login_required
+def comentario(com_id):
+    comentario = Comentario.query.get_or_404(com_id)
+    return render_template('comentario.html', titulo='Comentario', comentario=comentario)
+
+
+@app.route('/comentarios/nuevo/<int:preg_id>', methods=['GET', 'POST'])
+@login_required
+def postComentario(preg_id):
+    forma = CreateCommentForm() 
+    if forma.validate_on_submit():
+        comentario = Comentario(contenido=forma.contenido.data, likes=0, util=False, pregunta_id=preg_id, user_id=current_user.id)
+        db.session.add(comentario)
+        db.session.commit()
+        flash('Tu comentario se ha creado!', 'success')
+        return redirect(url_for('pregunta', preg_id=comentario.pregunta_id))
+    return render_template('nuevoComentario.html', forma=forma, titulo='Nuevo Comentario')
+
+
+@app.route('/comentarios/<int:com_id>/actualizar', methods=['GET','POST'])
+@login_required
+def actualizarComentario(com_id):
+    comentario = Comentario.query.get_or_404(com_id)
+    if comentario.autor != current_user:
+        abort(403)
+    forma = CreateCommentForm()
+    if forma.validate_on_submit():
+        comentario.contenido = forma.contenido.data
+        db.session.commit()
+        flash('Actualizamos tu comentario', 'success')
+        return redirect(url_for('pregunta', preg_id=comentario.pregunta_id))
+    elif request.method == 'GET':
+        # Llenar la forma con los datos actuales
+        forma.contenido.data = comentario.contenido
+        return render_template('nuevoComentario.html', forma=forma, titulo='Acualizar comentario',
+                               legenda='Cambia tu comentario')
+
+
+@app.route('/comentarios/<int:com_id>/eliminar', methods=['GET', 'POST'])
+@login_required
+def eliminarComentario(com_id):
+    comentario = Comentario.query.get_or_404(com_id)
+    if comentario.autor != current_user:
+        abort(403)
+    db.session.delete(comentario)
+    db.session.commit()
+    flash('Hemos eliminado tu comentario!', 'success')
+    return redirect(url_for('pregunta', preg_id=comentario.pregunta_id))
+
+
+@ app.route('/like/comentario/<int:com_id>', methods=['GET','POST'])
+def likeComentario(com_id):
+    comentario = Comentario.query.filter_by(id=com_id).first()
+    comentario.likes = comentario.likes + 1
+    db.session.commit()
+    return redirect(url_for('pregunta', preg_id=comentario.pregunta_id))
+
+@app.route('/sobre', methods=['GET'])
+def sobre():
+    return "QA UNAL"
+
+
+@ app.route('/cerrar_sesion', methods=['GET', 'POST'])
+def cerrarSesion():
+    logout_user()
+    return redirect(url_for('sobre'))
+
+
+@ app.route('/cuenta', methods=['GET', 'POST'])
+@login_required
+def miCuenta():
+    return render_template("miCuenta.html")
+
+
+@app.route('/usuario/preguntas')
+def misPreguntas():
+    page = request.args.get('page', default=1, type=int)
+    user = User.query.filter_by(id=current_user.id).first_or_404()
+    preguntas = Pregunta.query.filter_by(autor=user).paginate(page=page, per_page=10)
+    return render_template('home.html', titulo='Inicio', preguntas=preguntas)
